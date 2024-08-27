d0ebdd10cfee3e95224b28e4e6f6700da8619061
Garmii <miguelgar2001@gmail.com>
2024-08-27 20:24:42 +0200

    feat() flag tiles, vibrate on loose, game win condition and game loose condition, generate board bug solved

--- scripts/board.gd
+++ scripts/board.gd
@@ -2,7 +2,7 @@ extends TileMap
 
 @export var board_size_y : int = 10
 @export var board_size_x : int = 10
-@export var number_of_mines : int = 40
+@export var number_of_mines : int = 2
 
 signal flag_change(number_of_flags)
 signal game_lost
@@ -14,7 +14,7 @@ const ORIGINAL_TILE_SIZE : int = 16
 var screen_size : Vector2i 
 
 var is_game_finished : bool = false
-
+var number_of_flags : int = 0
 const CELLS = {
 	"1" : Vector2i(0,0),
 	"2" : Vector2i(1,0),
@@ -33,6 +33,7 @@ const CELLS = {
 
 var tiles_with_mines = []
 var cells_checked_recursively = []
+var tiles_with_flags = []
 
 func _ready():
 	screen_size = DisplayServer.window_get_size()
@@ -58,6 +59,9 @@ func load_board_configuration(screen_size: Vector2i):
 	var max_scale_factor_y: float = float(screen_size.y) / float(screen_size.x)
 	var max_scale_factor_x: float = float(screen_size.x) / float(screen_size.y)
 	
+	board_size_x -= 1
+	board_size_y -= 1
+	
 	if (float(board_size_y)/float(board_size_x) > max_scale_factor_y):
 		board_size_y = board_size_x*max_scale_factor_y
 		
@@ -81,10 +85,10 @@ func generate_board(board_size_x : int, board_size_y : int):
 
 func generate_mines():
 	for i in number_of_mines:
-		var mine_tile_coords = Vector2(randi_range(0,board_size_x),randi_range(0,board_size_y))
+		var mine_tile_coords = Vector2(randi_range(0,board_size_x -1),randi_range(0,board_size_y -1))
 		
 		while tiles_with_mines.has(mine_tile_coords):
-			mine_tile_coords = Vector2(randi_range(0,board_size_x),randi_range(0,board_size_y))
+			mine_tile_coords = Vector2(randi_range(0,board_size_x -1),randi_range(0,board_size_y -1))
 			
 		tiles_with_mines.append(mine_tile_coords)
 	print(tiles_with_mines)
@@ -94,6 +98,13 @@ func generate_mines():
 		set_cell(TILE_SET_LAYER,tile,TILE_SET_ID,CELLS.DEFAULT,1)
 
 func on_tile_pressed(coords : Vector2i):
+	if is_game_finished:
+		return
+	if tiles_with_flags.any(func(tile): return tile.x == coords.x && tile.y == coords.y):
+		return
+	if cells_checked_recursively.any(func(tile): return tile.x == coords.x && tile.y == coords.y):
+		return
+	
 	if tiles_with_mines.any(func(tile): return tile.x == coords.x && tile.y == coords.y):
 		loose(coords)
 		
@@ -102,7 +113,7 @@ func on_tile_pressed(coords : Vector2i):
 	handle_tile_data(coords,true)
 	
 func on_tile_hold(coords : Vector2i):
-	print("Mantener")
+	place_flag(coords)
 	
 func set_tile_cell(cell_coord, cell_type):
 	set_cell(TILE_SET_LAYER, cell_coord, TILE_SET_ID, CELLS[cell_type])
@@ -162,7 +173,6 @@ func get_surrounding_cells_to_check(current_cell: Vector2i) -> Array:
 
 func loose(coords: Vector2i):
 	var sw : bool = true
-	game_lost.emit()
 	is_game_finished = true
 	for tile : Vector2i in tiles_with_mines:
 		await get_tree().create_timer(0.01).timeout
@@ -171,5 +181,43 @@ func loose(coords: Vector2i):
 			sw = false
 		if !(tile == coords):
 			set_tile_cell(tile, "MINE")
+	Input.vibrate_handheld()
+	game_lost.emit()
+
+func place_flag(coords: Vector2i):
+	if is_game_finished:
+		return
+	var tile_data = get_cell_tile_data(TILE_SET_LAYER,coords)
+	var atlas_coords = get_cell_atlas_coords(TILE_SET_LAYER,coords)
+	var is_empty_cell = atlas_coords == Vector2i(2,2)
+	var is_flag_cell = atlas_coords == Vector2i(0,2)
 		
+	if is_flag_cell:
+		set_tile_cell(coords, "DEFAULT")
+		tiles_with_flags.erase(coords)
+		number_of_flags -= 1
+	if is_empty_cell:
+		if number_of_flags == number_of_mines:
+			return
+		set_tile_cell(coords, "FLAG")
+		tiles_with_flags.append(coords)
+		number_of_flags += 1
+		
+	flag_change.emit(number_of_flags)
+	print(coords, " flagged")
+	
+	var count = 0
+	for flag in tiles_with_flags:
+		for mine in tiles_with_mines:
+			if (flag.x == mine.x && flag.y == mine.y):
+				count += 1
+	
+	if count == tiles_with_mines.size():
+		win_game()
+		
+func win_game():
+	is_game_finished = true
+	game_won.emit()
+	
+	
 	
