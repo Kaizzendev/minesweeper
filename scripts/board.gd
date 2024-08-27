01d2c896702d813bb9790f5f53238ab2e84d491b
kaizzenn <kaizendev@gmail.com>
2024-08-27 02:02:03 +0200

    feat() generate_board_recursively

--- scripts/board.gd
+++ scripts/board.gd
@@ -2,7 +2,7 @@ extends TileMap
 
 @export var board_size_y : int = 10
 @export var board_size_x : int = 10
-@export var number_of_mines : int = 10
+@export var number_of_mines : int = 40
 
 const TILE_SET_ID : int = 0
 const TILE_SET_LAYER : int = 0
@@ -26,9 +26,8 @@ const CELLS = {
 }
 
 var tiles_with_mines = []
-var touch_position = Vector2()
-var hold_duration = 5  # en segundos
-# Called when the node enters the scene tree for the first time.
+var cells_checked_recursively = []
+
 func _ready():
 	screen_size = DisplayServer.window_get_size()
 	load_board_configuration(screen_size)
@@ -72,6 +71,7 @@ func generate_board(board_size_x : int, board_size_y : int):
 			set_cell(TILE_SET_LAYER,Vector2i(x,y),TILE_SET_ID,CELLS.DEFAULT)
 	
 	generate_mines()
+
 func generate_mines():
 	for i in number_of_mines:
 		var mine_tile_coords = Vector2(randi_range(0,board_size_x),randi_range(0,board_size_y))
@@ -80,7 +80,7 @@ func generate_mines():
 			mine_tile_coords = Vector2(randi_range(0,board_size_x),randi_range(0,board_size_y))
 			
 		tiles_with_mines.append(mine_tile_coords)
-		print(tiles_with_mines)
+	print(tiles_with_mines)
 		
 	for tile in tiles_with_mines:
 		erase_cell(TILE_SET_LAYER,tile)
@@ -89,6 +89,66 @@ func generate_mines():
 func on_tile_pressed(coords : Vector2i):
 	if tiles_with_mines.any(func(tile): return tile.x == coords.x && tile.y == coords.y):
 		print("Mine!!!!")
+		
+	cells_checked_recursively.append(coords)
+	
+	handle_tile_data(coords,true)
 	
 func on_tile_hold(coords : Vector2i):
 	print("Mantener")
+	
+func set_tile_cell(cell_coord, cell_type):
+	set_cell(TILE_SET_LAYER, cell_coord, TILE_SET_ID, CELLS[cell_type])
+
+func handle_tile_data(cell_coord: Vector2i, should_stop_after_mine: bool = false):
+	var tile_data = get_cell_tile_data(TILE_SET_LAYER,cell_coord)
+	
+	if tile_data == null:
+		return
+		
+	
+	var tile_has_mine = tile_data.get_custom_data("has_mine") 
+	
+	if tile_has_mine && should_stop_after_mine:
+		return
+	
+	var mine_count = get_surrounding_cells_mine_count(cell_coord)
+	
+	if mine_count == 0:
+		set_tile_cell(cell_coord, "EMPTY")
+		var surrounding_cells = get_surrounding_cells(cell_coord)
+		for cell in surrounding_cells:
+			handle_surrounding_cell(cell)
+	else:
+		set_tile_cell(cell_coord, "%d" % mine_count)
+
+func handle_surrounding_cell(cell_coord: Vector2i):
+	if cells_checked_recursively.has(cell_coord):
+		return
+		
+	cells_checked_recursively.append(cell_coord)
+	handle_tile_data(cell_coord)
+
+func get_surrounding_cells_mine_count(cell_coord: Vector2i) -> int:
+	var mine_count = 0
+	var sourrounding_cells = get_surrounding_cells_to_check(cell_coord)
+	
+	for cell in sourrounding_cells:
+		var tile_data = get_cell_tile_data(TILE_SET_LAYER,cell)
+		if tile_data and tile_data.get_custom_data("has_mine"):
+			mine_count += 1
+			
+	return mine_count
+	
+func get_surrounding_cells_to_check(current_cell: Vector2i) -> Array:
+	var target_cell
+	var surrounding_cells = []
+	
+	for y in 3:
+		for x in 3:
+			if x == 1 and y == 1:
+				continue
+			target_cell = current_cell + Vector2i(x - 1, y - 1)
+			surrounding_cells.append(target_cell)
+	
+	return surrounding_cells
