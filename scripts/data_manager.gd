ce424934b66de0087e3bee86c0be568855024d20
Garmii <93149120+Garmii@users.noreply.github.com>
2024-09-02 20:44:40 +0200

    feat() save best time

--- scripts/data_manager.gd
+++ scripts/data_manager.gd
@@ -1,1 +1,20 @@
 extends Node
+
+var file_path : String = "user://save.tres"
+var data : Dictionary = {
+	time_record = 0
+}
+func save_data(content):
+	var file = FileAccess.open(file_path, FileAccess.WRITE)
+	file.store_var(JSON.stringify(content),true)
+	print("Loaded data:", JSON.stringify(content) )
+
+func load_data():
+	if FileAccess.file_exists(file_path):
+		var file = FileAccess.open(file_path, FileAccess.READ)
+		var loadParam=JSON.parse_string(file.get_var(true));
+		file.close()
+		if loadParam == null:
+			print("No se han encontrado datos")
+		print("Saved data: ", loadParam)
+		return loadParam
--- scripts/win_screen.gd
+++ scripts/win_screen.gd
@@ -4,12 +4,13 @@ class_name WinScreen
 
 @onready var current_time_label = $PanelContainer/Panel/VBoxContainer/CurrentTimeLabel
 @onready var personal_best_label = $PanelContainer/Panel/VBoxContainer/PersonalBestLabel
+@onready var new_record_label = $PanelContainer/Panel/VBoxContainer/NewRecordLabel
 
 @export var hud : HUD
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -25,6 +26,23 @@ func _on_play_again_button_pressed():
 	get_tree().reload_current_scene()
 
 func game_won():
+	if  DataManager.load_data() == null:
+		DataManager.data = {time_record = hud.total_time_seconds} 
+		DataManager.save_data(DataManager.data)
+	DataManager.data = DataManager.load_data()
+	var current_record = DataManager.data.get("time_record")
+	if hud.total_time_seconds <= current_record:
+		new_record_label.show()
+		new_record_label.text = "New Record!! " + hud.timer_count_label.text
+		personal_best_label.text = "Personal best: " + hud.timer_count_label.text
+		DataManager.data = {time_record = hud.total_time_seconds} 
+		DataManager.save_data(DataManager.data)
+	else:
+		fill_personal_label(current_record)
 	current_time_label.text = "Your time was: " + hud.timer_count_label.text
 	show()
 	
+func fill_personal_label(total_time_seconds):
+	var minutes = int(total_time_seconds / 60)
+	var seconds = total_time_seconds - minutes * 60
+	personal_best_label.text = "Personal best: " + "%02d:%02d" % [minutes,seconds]
