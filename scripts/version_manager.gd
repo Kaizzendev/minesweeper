8fb5118ca17b3233e5ad7d1e6fb3c91d5f031bfd
Garmii <93149120+Garmii@users.noreply.github.com>
2024-09-04 20:13:38 +0200

    feat() added launcher icons

--- scripts/version_manager.gd
+++ scripts/version_manager.gd
@@ -4,7 +4,7 @@ const FILE_PATH = "res://version.txt"
 
 func get_version_number():
 	var file = FileAccess.open(FILE_PATH, FileAccess.READ)
-	var content=file.get_as_text()
+	var content = file.get_as_text()
 	file.close()
 		
 	return content
